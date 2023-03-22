fd5cb0cbf6d23433a84172e106145a0db7086009
Demilade362 <ademolademilade362@gmail.com>
2023-03-22 01:00:48 +0100

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,35 +1,40 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * times_table - prints the 9 times table, starting with 0
+ * times_table - prints the 9 table
+ *
+ *  Description: prints all the nine table
+ * Return: Always (0).
  */
-void time_table(void)
+
+void times_table(void)
 {
-int i, j, k;
-for (i = 0; i < 10; i++)
-{
-for (j = 0; j < 10; j++)
-{
-k = j * i;
-if (j == 0)
-{
-_putchar(k + '0');
+    int i, j, product;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 10; j++)
+        {
+            if (j == 0)
+            {
+                _putchar(48);
+                continue;
+            }
+            product = i * j;
+            _putchar(',');
+            _putchar(' ');
+            if (product >= 10)
+            {
+                _putchar(product / 10 + 48);
+                _putchar(product % 10 + 48);
+            }
+            else
+            {
+                _putchar(' ');
+                _putchar(product + 48);
+            }
+        }
+        _putchar('\n');
+    }
 }
-if (k < 10 && j != 0)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(k + '0');
-}
-else if (k >= 10)
-{
-_putchar(',');
-_putchar(' ');
-_putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
-}
-}
-_putchar('\n');
-}
-}
+
